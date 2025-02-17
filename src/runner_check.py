db6b56ddc6e5ab7e47f0adc3f3dded0b3c2a5501
pouyasadeghi <pouyasadeghi2012@gmail.com>
2025-02-17 19:20:09 +0800

    Initiate README.md

--- src/runner_check.py
+++ src/runner_check.py
@@ -1,27 +1,4 @@
-from time import perf_counter
-import numpy as np
-
-from system.runner import Runner
-
-def benchmark_runner(path, iterations=10):
-    runtimes = []
-    for _ in range(iterations):
-        start_time = perf_counter()
-        runner_instance = Runner(path, "")
-        runner_instance.run()
-        end_time = perf_counter()
-        print(f"Runtime: {end_time - start_time:.3f} seconds")
-        assert runner_instance.history["solver_result"]["is_sat"] == "sat", "Failed to satisfy the constraints"
-
-        runtimes.append(end_time - start_time)
-    mean_runtime = np.mean(runtimes)
-    std_runtime = np.std(runtimes)
-
-    print(f"Mean Runtime: {mean_runtime:.9f} seconds")
-    print(f"Standard Deviation of Runtime: {std_runtime:.9f} seconds")
-    print(f"Mean Runtime: {mean_runtime:.3f} ± {std_runtime:.3f} seconds")
-
-    return mean_runtime, std_runtime
+from system import benchmark_runner
 
 if __name__ == "__main__":
     config_file = "./benchmark/random_walk_verification_3.json"
